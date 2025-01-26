f060ffad9b2b8983341d71fce81416e6758f140f
kiko923 <75443014+kiko923@users.noreply.github.com>
2025-01-26 17:05:40 +0800

    Update worker.js

--- functions/worker.js
+++ functions/worker.js
@@ -98,10 +98,7 @@ async function handleRedirect(request) {
 addEventListener('fetch', event => {
   const url = new URL(event.request.url);
   
-  if (url.pathname === '/') {
-    // 根路径的请求
-    event.respondWith(fetch(new Request('https://kiko923.github.io/MyUrls/public/')));
-  } else if (url.pathname === '/short') {
+    if (url.pathname === '/short') {
     // 创建短链接
     event.respondWith(handleRequest(event.request));
   } else if (url.pathname.startsWith('/')) {
